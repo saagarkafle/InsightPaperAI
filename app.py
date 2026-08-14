a376b268e47d2c49168383236872f2c7ae23a396
Sagar Kafle <kafleysaagar@gmail.com>
2026-08-14 10:32:01 +0100

    Add Streamlit Cloud secrets synchronization safeguard

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import os
 import streamlit as st
+
+# Sync Streamlit Cloud secrets to environment variables if deployed on Streamlit Cloud
+for secret_key in ["PINECONE_API_KEY", "GROQ_API_KEY"]:
+    if secret_key not in os.environ and secret_key in st.secrets:
+        os.environ[secret_key] = st.secrets[secret_key]
+
 
 from src.mvc import AppController, AppModel, AppView
 
